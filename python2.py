a7cc82e082b1abcd8a51a098e42a9316c5bd0e6e
BeroCollege <Xdrr9728!>
2025-02-16 22:13:39 +0000

    calculate average of 5 numbers

---
+++ python2.py
@@ -0,0 +1,10 @@
+usrinput = int(input("Please enter a number: "))
+usrinput2 = int(input("Please enter a number: "))
+usrinput3 = int(input("Please enter a number: "))
+usrinput4 = int(input("Please enter a number: "))
+usrinput5 = int(input("Please enter a number: "))
+
+total = usrinput + usrinput2 + usrinput3 + usrinput4 + usrinput5
+average = total / 5
+
+print(int(average))
