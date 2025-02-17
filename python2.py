7f5b693bc77b4c0e9b25cd0cdfe47b5be34a2d93
Jason Garcia <c22516126@mytudublin.ie>
2025-02-17 23:01:09 +0000

    largest and smallest variables

--- python2.py
+++ python2.py
@@ -1,10 +1,43 @@
 usrinput = int(input("Please enter a number: "))
-usrinput2 = int(input("Please enter a number: "))
-usrinput3 = int(input("Please enter a number: "))
-usrinput4 = int(input("Please enter a number: "))
-usrinput5 = int(input("Please enter a number: "))
 
+small = usrinput
+large = usrinput
+
+usrinput2 = int(input("Please enter a number: "))
+
+if usrinput2 < small:
+    small = usrinput2
+
+if usrinput2 > large:
+    large = usrinput2
+
+usrinput3 = int(input("Please enter a number: "))
+
+if usrinput3 < small:
+    small = usrinput2
+
+if usrinput3 > large:
+    large = usrinput2
+    
+usrinput4 = int(input("Please enter a number: "))
+
+if usrinput4 < small:
+    small = usrinput2
+
+if usrinput4 > large:
+    large = usrinput2
+    
+usrinput5 = int(input("Please enter a number: "))
+if usrinput5 < small:
+    small = usrinput2
+
+if usrinput5 > large:
+    large = usrinput2
+    
 total = usrinput + usrinput2 + usrinput3 + usrinput4 + usrinput5
 average = total / 5
 
+
 print(int(average))
+print(int(small))
+print(int(large))
